33a7fb1a2c3783b6b370ec527642d62df449ba55
SACHIN GADHARI <37650415+GadhariSachin@users.noreply.github.com>
2024-02-28 18:50:22 +0530

    Create Selection_Sort.js
    Added comments selection sort

---
+++ Selection_Sort.js
@@ -0,0 +1,9 @@
+// Selection Sort in JS
+
+/*
+---------------
+Selection Sort (Read More here - https://www.geeksforgeeks.org/selection-sort/)
+---------------
+Selection sort is a simple and efficient sorting algorithm that works by repeatedly selecting the smallest (or largest) element from the unsorted portion of the list and moving it to the sorted portion of the list. 
+*/ 
+
