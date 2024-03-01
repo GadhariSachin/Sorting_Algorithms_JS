5ee30fdce0e21ee413b7ea43048a293f5daddc8f
sachingadhari <37650415+GadhariSachin@users.noreply.github.com>
2024-03-01 20:13:49 +0530

    Added Quick Sort Code

--- Quick_Sort.js
+++ Quick_Sort.js
@@ -16,14 +16,74 @@ Time Complexity -  O(N Log(N)), O ( N log (N)), O(N^2)
 
 */
 
+/* Quick Way of doing Quick Sort but it uses extra auxillary space for storing left and right array */
+function QuickSortWithExtraSpace(unSortedArray) {
+  if (unSortedArray.length <= 1) return unSortedArray;
 
-function QuickSort(unSortedArray) {
-  
- 
-  return unSortedArray;
+  let left = [],
+    right = [];
+
+  const pivot = unSortedArray[unSortedArray.length - 1];
+
+  for (let i = 0; i < unSortedArray.length - 1; i++) {
+    if (unSortedArray[i] > pivot) {
+      right.push(unSortedArray[i]);
+    } else {
+      left.push(unSortedArray[i]);
+    }
+  }
+
+  return [
+    ...QuickSortWithExtraSpace(left),
+    pivot,
+    ...QuickSortWithExtraSpace(right),
+  ];
 }
 
-const inputArray = [5, 8, 3, 1];
+function swap(arr, i, j) {
+  const temp = arr[i];
+  arr[i] = arr[j];
+  arr[j] = temp;
+}
+
+const helper=(arr,start,end)=>{
+  if(start>=end ){
+      return arr
+  }
+  const pivotIndex = Math.floor(Math.random() * (end - start) + start)
+  
+  swap(arr, start, pivotIndex);
+  
+  let small=start;
+
+  for (let big = start + 1; big <= end; big++) {
+      if (arr[start] > arr[big]) {
+          small++;
+          swap(arr,small, big);
+      }
+  }
+
+  swap(arr,small,start)
+  
+  helper(arr,start,small-1)
+
+  helper(arr,small+1, end)
+
+  return arr
+  
+}
+
+function QuickSort(arr) {
+
+  const start = 0,
+    end = arr.length - 1;
+
+  return helper(arr, start, end);
+}
+
+const inputArray = [50, 8, 3, 1, 100, 50, 12];
+
+// const sortedArray = QuickSortWithExtraSpace(inputArray);
 
 const sortedArray = QuickSort(inputArray);
 
