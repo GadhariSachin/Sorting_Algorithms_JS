c7f497622d257ab79a12ecba87fe7513463098ba
sachingadhari <37650415+GadhariSachin@users.noreply.github.com>
2024-03-01 16:29:29 +0530

    Added merge sort code

--- Merge_Sort.js
+++ Merge_Sort.js
@@ -14,14 +14,67 @@ Time Complexity -  O(N LogN)
 
 */
 
+function splitHelper(unSortedArray, start, end) {
+
+  // Base Case - Leaf Level Worker
+  if (start >= end) return;
+
+  // Recursie Case - Internal Node
+  const mid = parseInt((start + end) / 2);
+  splitHelper(unSortedArray, start, mid);
+  splitHelper(unSortedArray, mid + 1, end);
+  mergeHelper(unSortedArray, start, mid, end);
+}
+  
+
+function mergeHelper(unSortedArray, start, mid, end){
+  let i = start; 
+  let j = mid + 1;
+  let auxArray = [];
+
+  while (i <= mid && j <= end) {
+    if (unSortedArray[i] < unSortedArray[j]) {
+      auxArray.push(unSortedArray[i]);
+      i++;
+    } else {
+      auxArray.push(unSortedArray[j]);
+      j++;
+    }
+  }
+
+  while (i <= mid) {
+    auxArray.push(unSortedArray[i]);
+    i++;
+  }
+
+  while (j <= end) {
+    auxArray.push(unSortedArray[j]);
+    j++;
+  }
+
+  // Copy sorted elements back to the original array
+  for (let k = 0; k < auxArray.length; k++) {
+    unSortedArray[start + k] = auxArray[k];
+  }
+
+  return unSortedArray;
+}
+
+
 function MergeSort(unSortedArray) {
   
-  
+  const len = unSortedArray.length;
+
+  const start = 0;
+
+  const end = len - 1;
+
+  splitHelper(unSortedArray, start, end);
   
   return unSortedArray;
 }
 
-const inputArray = [5, 8, 3];
+const inputArray = [5, 8, 3, 1];
 
 const sortedArray = MergeSort(inputArray);
 
