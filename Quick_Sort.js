7fd3c919e2356e7c23a0916fde7430811f2ef16b
sachingadhari <37650415+GadhariSachin@users.noreply.github.com>
2024-03-01 16:52:27 +0530

    Added Quick Sort

---
+++ Quick_Sort.js
@@ -0,0 +1,30 @@
+// Quick Sort in JS
+
+/*
+---------------
+Quick Sort (Read More here - https://www.geeksforgeeks.org/quick-sort/)
+---------------
+QuickSort is a sorting algorithm based on the Divide and Conquer algorithm that picks an element as a pivot and partitions 
+the given array around the picked pivot by placing the pivot in its correct position in the sorted array.
+
+
+
+
+----------------
+Time Complexity -  O(N Log(N)), O ( N log (N)), O(N^2)
+----------------
+
+*/
+
+
+function QuickSort(unSortedArray) {
+  
+ 
+  return unSortedArray;
+}
+
+const inputArray = [5, 8, 3, 1];
+
+const sortedArray = QuickSort(inputArray);
+
+console.log("Sorted Array using Quick Sort ::: ", sortedArray);
